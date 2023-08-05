039c761702f27a2e7ca1d2eacc96118938dca1c5
jordanalexandermeyer <jordanalexandermeyer@gmail.com>
2023-08-05 16:22:07 -0700

    added json parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   if (
     req.query["hub.mode"] == "subscribe" &&
